f32e42f12ef755c6011e89448eea524f0dec38bb
Sophie Yeh <sophie256@gmail.com>
2022-08-12 18:57:50 -0400

    added missing fcns

--- python-algo/algo_strategy.py
+++ python-algo/algo_strategy.py
@@ -172,7 +172,15 @@ class AlgoStrategy(gamelib.AlgoCore):
         # Now spawn demolishers next to the line
         # By asking attempt_spawn to spawn 1000 units, it will essentially spawn as many as we have resources for
         game_state.attempt_spawn(DEMOLISHER, [24, 10], 1)
-
+    def detect_enemy_unit(self, game_state, unit_type=None, valid_x = None, valid_y = None):
+        total_units = 0
+        for location in game_state.game_map:
+            if game_state.contains_stationary_unit(location):
+                for unit in game_state.game_map[location]:
+                    if unit.player_index == 1 and (unit_type is None or unit.unit_type == unit_type) and (valid_x is None or location[0] in valid_x) and (valid_y is None or location[1] in valid_y):
+                        total_units += 1
+        return total_units
+        
     def on_action_frame(self, turn_string):
         """
         This is the action frame of the game. This function could be called
